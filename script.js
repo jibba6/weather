71290a55ff6d10aa35b3d0dbb25d6063f6193bda
James Jabar <jabar.james.jj@gmail.com>
2022-09-23 13:54:21 -0400

    almost done

---
+++ script.js
@@ -0,0 +1,70 @@
+var apiKey = "75a4001fc2cc51b43800eaa273e443f3"
+
+function getCity (){
+    var city = $("#searchInput").val()
+    var url = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`
+    fetch(url).then(response => response.json())
+    .then(data => {
+        getWeather(data[0].lat, data[0].lon);
+        getFiveDay(data[0].lat, data [0].lon);
+})
+        
+};
+
+
+
+function getWeather ( lat , lon){
+    var url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
+    fetch(url).then(response => response.json())
+    .then(data => {
+        console.log(data)
+        var currentDayEl = $("#currentDay")
+        var html = `<div class="card" style="width: 18rem;">
+        <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${data.name}</h5>
+          <p class="card-text">Temp: ${data.main.temp} </p>
+          <p class="card-text">Humidity: ${data.main.humidity} </p>
+          <p class="card-text">Wind Speed: ${data.wind.speed} </p>
+        </div>
+      </div>`
+      currentDayEl.html(html)
+})
+};
+
+
+function getFiveDay ( lat , lon){
+    var url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
+    fetch(url).then(response => response.json())
+    .then(data => {
+        console.log(data)
+        var html = []
+        var fiveDayEl = $("#fiveDay")
+        for (let i = 15; i< data.list.length; i+=8){
+            
+        html.push(`<div class="card" style="width: 18rem;">
+        <img src="http://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png" class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Date: ${moment.unix(data.list[i].dt).format("DD/MM/YYYY hh a")} </p>
+          <p class="card-text">Temp: ${data.list[i].main.temp} </p>
+          <p class="card-text">Humidity: ${data.list[i].main.humidity} </p>
+          <p class="card-text">Wind Speed: ${data.list[i].wind.speed} </p>
+        </div>
+      </div>`
+    )}
+      fiveDayEl.html(html.join(""))
+    })
+};
+
+
+
+
+
+
+
+
+
+
+
+
+$("#search").on("click",getCity);
